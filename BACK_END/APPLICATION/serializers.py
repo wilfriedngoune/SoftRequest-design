a66ea6a93ca8d079fe3d0cd0f6ce307f9748aea9
levinne <clemence.djeumeni@facsciences-uy1.cm>
2022-01-09 22:49:29 +0100

    ajout api de connection

--- BACK_END/APPLICATION/serializers.py
+++ BACK_END/APPLICATION/serializers.py
@@ -1,29 +1,77 @@
 from rest_framework import serializers
 from.models import*
 
-""" classe"""
+""" """
 class Administration(serializers.ModelSerializer):
     class Meta:
         model: Administration
         fields = '__all__'
 
-class Etudiant(serializers.ModelSerializer):
+class EtudiantSerializer(serializers.ModelSerializer):
     class Meta:
         model: Etudiant
         fields = '__all__'
 
-class Grade(serializers.ModelSerializer):
+class GradeSerializer(serializers.ModelSerializer):
     class Meta:
         model: Grade
         fields = '__all__'
 
-class Reponse(serializers.ModelSerializer):
+class ReponseSerializer(serializers.ModelSerializer):
     class Meta:
         model: Reponse
         fields = '__all__'
 
-class Envoyer(serializers.ModelSerializer):
+class EnvoyerSerializer(serializers.ModelSerializer):
     class Meta:
         model: Envoyer
         fields = '__all__'
 
+class DemandeSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Demande
+        fields = '_all_'
+
+class Abscence_noteSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Abscence_note
+        fields = '_all_'
+
+
+class Activation_de_matriculeSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Activation_de_matricule
+        fields = '_all'
+
+class Abscence_payementSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Abscence_payement
+        fields = '_all'
+
+
+class PieceJointeSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = PieceJointe
+        fields = '_all'
+
+class Bloquage_matriculeSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Bloquage_matricule
+        fields = '_all'
+
+
+class Changement_filiereSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Changement_filiere
+        fields = '_all'
+
+
+class Note_erroneeSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Note_erronee
+        fields = '_all'
+
+class Information_eroneeSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Information_eronee
+        fields = '_all'
