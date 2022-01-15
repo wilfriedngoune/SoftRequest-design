2720b710989f657509e9aab405e91b55f411c5fc
levinne <clemence.djeumeni@facsciences-uy1.cm>
2022-01-15 17:13:02 +0100

    formating db

--- BACK_END/settings.py
+++ BACK_END/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(r9g4q0p9q0#e5*wsyj7cd+n(p(ad1t=h_&25jts_9@+ok8mjz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["softrequest.herokuapp.com", "127.0.0.1"]
 
