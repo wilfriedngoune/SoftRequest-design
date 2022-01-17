8a331a9d3230ec5a1f10c1110f469979e5f651f9
levinne <clemence.djeumeni@facsciences-uy1.cm>
2022-01-17 18:35:34 +0100

    cross_origin

--- BACK_END/settings.py
+++ BACK_END/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["softrequest.herokuapp.com", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'APPLICATION',
     'rest_framework',
     'django.contrib.admin',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'BACK_END.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
