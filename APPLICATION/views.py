255b01154e6e0b17a5e5660bd734992a5c409564
levinne <clemence.djeumeni@facsciences-uy1.cm>
2022-01-20 21:33:25 +0100

    Non prise en compte de la validite des info

--- APPLICATION/views.py
+++ APPLICATION/views.py
@@ -46,18 +46,13 @@ class AdministrationList(CreateModelMixin, ListModelMixin, GenericViewSet):
 @api_view(['POST'])
 def insertUser(request):
     if request.method == 'POST':
-        etudiant_data = request.data
+        etudiant_data = request.post
         etudiants_serializer = EtudiantSerializer(data = etudiant_data)
-        if etudiants_serializer.is_valid():
-            etudiants_serializer.save()
-            return Response({
-                "donnee" : etudiants_serializer.data, 
-                "Message" : "Added Successfully"
-                })
+        etudiants_serializer.save()
         return Response({
-                "donnee" : etudiants_serializer.data, 
-                "Message" : "Failed to request .."
-                })
+            "donnee" : etudiants_serializer, 
+            "Message" : "Added Successfully"
+            })
 
 
 
