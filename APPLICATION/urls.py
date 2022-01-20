9600ad4396abd2d497f12821bde4008c0659a695
levinne <clemence.djeumeni@facsciences-uy1.cm>
2022-01-20 18:44:40 +0100

    Ajout de la version repath partout dans les urls

--- APPLICATION/urls.py
+++ APPLICATION/urls.py
@@ -1,19 +1,18 @@
 from venv import create
 from django.urls import re_path
-from django.urls import path
 from .import views
 from .views import *
 urlpatterns = [
     re_path('insert/user/', views.insertUser),
-    path('etudiants/', StudentList.as_view({'get':'list','post':'create'})),
-    path('user/', UserList.as_view({'get':'list','post':'create'})),
-    path('administration/', AdministrationList.as_view({'get':'list','post':'create'})),
-    path('etudiant/<str:matricule>', views.getEtudiant),
-    path('administration/<str:email_pass>', views.getAdministration),
-    #path('etudiants/', views.allEtudiant),
-    path('addetudiants/', views.addEtudiant),
-    path('administrations/', views.allAdministration),
-    path('addadministration/', views.addAdministration),
+    re_path('etudiants/', StudentList.as_view({'get':'list','post':'create'})),
+    re_path('user/', UserList.as_view({'get':'list','post':'create'})),
+    re_path('administration/', AdministrationList.as_view({'get':'list','post':'create'})),
+    re_path('etudiant/<str:matricule>', views.getEtudiant),
+    re_path('administration/<str:email_pass>', views.getAdministration),
+    #re_path('etudiants/', views.allEtudiant),
+    re_path('addetudiants/', views.addEtudiant),
+    re_path('administrations/', views.allAdministration),
+    re_path('addadministration/', views.addAdministration),
 
 
     #Mes trucs de test (ca va marcher, jai confiance)
