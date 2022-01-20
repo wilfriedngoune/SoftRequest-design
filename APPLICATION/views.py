a89a74a9e09d0bf11d0041098ad145b3fd5152b4
levinne <clemence.djeumeni@facsciences-uy1.cm>
2022-01-20 21:37:36 +0100

    Non prise en compte de la validite des info

--- APPLICATION/views.py
+++ APPLICATION/views.py
@@ -50,7 +50,7 @@ def insertUser(request):
         etudiants_serializer = EtudiantSerializer(data = etudiant_data)
         etudiants_serializer.save()
         return Response({
-            "donnee" : etudiants_serializer, 
+            "donnee" : etudiant_data, 
             "Message" : "Added Successfully"
             })
 
